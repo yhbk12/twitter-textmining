de091c4b23bfa3b51d62394c131079a7c5e7fd53
Yusuf Khaled <yusufkhaled@live.ca>
2018-05-04 15:47:55 -0400

    Updated authentication parameters

--- search_twitter_query.py
+++ search_twitter_query.py
@@ -82,24 +82,11 @@ def clean_tweet_text(tweet):
 def search_twitter_return_ev(query, start_date, end_date):
 
 
-    # Dhruv's Twitter API authentication
-    consumer_key = 'QyPohWOnU5rWBj0p8eitISxZm'
-    consumer_secret = 'Bic1EnxYGOnaZZaNiSz0xW0Kb3Y4RGVhqbfriVP3dgm2xdj2Ln'
-    access_token = '383059399-Nv6Z94gW7fMReiyKLGKANTFYay14tFvrB1Ut8c9s'
-    access_token_secret = 'BsM3FhhEkkBYe7q1LPTAHQa1HyZKmhjVkaxdA5hJefwGy'
-
-    # Yusuf's Twitter API authentication
-    # consumer_key = 'GyfyFJEkU6cyGBq0PPLjHlvz0'
-    # consumer_secret = 'q3ghkBA8i1qheGFFnpd5mmCmlAlrNIk02wqTqeoQ2gERHiwqLw'
-    # access_token = '855727868-h0MenCCakLLaz6engeaIm2mh77j3uoOnN5DIXV07'
-    # access_token_secret = 'c4TWPLTCmdx8ijhXS3gkH59Wcv8PGJ8BUFDTFXfT6hMiS'
-
-    # Matt's Twitter API authentication
-    # consumer_key = '1iQ8rHkTabqFSEaIujAHqahTW'
-    # consumer_secret = 'KxnRmci0bkWpoeFF6lSyR8lzlNmutwDIaMZ6vkXF5moh68vS6v'
-    # access_token = '701880260678148096-GkJ6CXuCFLjQYKnQdYl13QZI0Lk49KU'
-    # access_token_secret = 'N5rwNAW7Su20FEZW741R0T992vg2xGwfqBNZzNC5FdMal'
-
+    # Twitter API authentication
+    consumer_key = 'GyfyFJEkU6cyGBq0PPLjHlvz0'
+    consumer_secret = 'q3ghkBA8i1qheGFFnpd5mmCmlAlrNIk02wqTqeoQ2gERHiwqLw'
+    access_token = '855727868-h0MenCCakLLaz6engeaIm2mh77j3uoOnN5DIXV07'
+    access_token_secret = 'c4TWPLTCmdx8ijhXS3gkH59Wcv8PGJ8BUFDTFXfT6hMiS'
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
